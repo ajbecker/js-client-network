e43c9ad292aeb5605b424013544be205a08be923
Adam Becker <ajbecker@gmail.com>
2012-02-11 16:12:52 -0800

    test

--- app.js
+++ app.js
@@ -34,5 +34,4 @@ app.configure('production', function(){
 app.get('/', routes.index);
 
 app.listen(8081);
-
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
