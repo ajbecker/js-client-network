c50ad5e3cd3c1477dbc2ecb3c4c297a786a87dac
Adam Becker <ajbecker@gmail.com>
2012-02-23 17:08:27 -0700

    updated repository to current code

--- app.js
+++ app.js
@@ -3,35 +3,37 @@
  * Module dependencies.
  */
 
-var express = require('express')
-  , routes = require('./routes');
-
-var app = module.exports = express.createServer();
+var express = require('express'), 
+	routes = require('./routes'),
+	socketListener = require("./socketListener"),
+	
+	// define the application and module exports.
+	app = module.exports = express.createServer();
 
 // Configuration
-
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(express.cookieParser());
-  app.use(express.session({ secret: 'your secret here' }));
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
+app.configure(function () {
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'jade');
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(express.cookieParser());
+	app.use(express.session({ secret: 'your secret here' }));
+	app.use(app.router);
+	app.use(express['static'](__dirname + '/public'));
 });
 
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+app.configure('development', function () {
+	app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
-app.configure('production', function(){
-  app.use(express.errorHandler());
+app.configure('production', function () {
+	app.use(express.errorHandler());
 });
 
 // Routes
-
 app.get('/', routes.index);
 
+// listen to the open port and open the socket.io connection.
 app.listen(8081);
+socketListener.listen(app);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
---
+++ socketListener.js
@@ -0,0 +1,53 @@
+
+var io = require("socket.io"),
+	_ = require("underscore");
+
+module.exports.listen = function (app) {
+	var sio = io.listen(app),
+		sockets = {},
+		metaData = {
+			connections: 0,
+			clients: {},
+			lastMessageTime: null,
+			lastMessageFrom: null
+		},
+		fnSendMessage = function (sockId, msg) {
+			var outgoingMsg,
+				disconnected = [],
+				connected = [];
+			metaData.lastMessageTime = new Date().toString();
+			metaData.lastMessageFrom = sockId;
+			_.each(sockets, function (sock, id) {
+				console.log("Checking for connection on " + id);
+				(sock.disconnected ? disconnected : connected).push(id);
+			});
+			_.each(disconnected, function (id) {
+				delete sockets[id];
+				delete metaData.clients[id];
+				metaData.connections -= 1;
+			});
+			_.each(connected, function (id) {
+				console.log("Sending message");
+				outgoingMsg = { meta: metaData, data: msg };
+				console.log(outgoingMsg);
+				sockets[id].emit("msg", outgoingMsg);
+			});
+		};
+		
+	sio.sockets.on("connection", function (socket) {
+		sockets[socket.id] = socket;
+		metaData.clients[socket.id] = socket.manager.handshaken[socket.id].address;
+		metaData.connections += 1;
+		fnSendMessage(socket.id, socket.id + " connected.");
+		
+		socket.on("disconnect", function () {
+			delete sockets[socket.id];
+			delete metaData.clients[socket.id];
+			metaData.connections -= 1;
+			fnSendMessage(socket.id, socket.id + " disconnected.");
+		});
+		socket.on("msg", function (msg) {
+			fnSendMessage(socket.id, msg);
+		});
+	});
+};
